3d9687920e7065b04a95f342444b13e6c13c1416
Bhushan Shah <bhush94@gmail.com>
2020-03-16 18:42:34 +0530

    add implementation of the approver
    currently dialer application behaves as both approver and the handler
    for call channels, we want to split this functionality in approver
    service which handles the call notification and acceptance/rejection and
    the handler which does actual handling of call.

---
+++ approver/callchannelapprover.cpp
@@ -0,0 +1,108 @@
+/*
+ * <one line to give the library's name and an idea of what it does.>
+ * Copyright 2020  <copyright holder> <email>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) version 3, or any
+ * later version accepted by the membership of KDE e.V. (or its
+ * successor approved by the membership of KDE e.V.), which shall
+ * act as a proxy defined in Section 6 of version 3 of the license.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "callchannelapprover.h"
+
+#include <KLocalizedString>
+
+#include <TelepathyQt/PendingReady>
+#include <TelepathyQt/ChannelClassSpec>
+#include <TelepathyQt/ClientRegistrar>
+#include <TelepathyQt/CallChannel>
+
+CallChannelApprover::CallChannelApprover(const Tp::CallChannelPtr & channel, QObject *parent)
+    : ChannelApprover(parent)
+{
+    if (!channel.isNull()) {
+        Tp::PendingReady *pendingReady = channel->becomeReady(Tp::Features()
+                                                            << Tp::CallChannel::FeatureCore
+                                                            << Tp::CallChannel::FeatureCallState);
+        m_Channels[pendingReady] = channel;
+        connect(pendingReady, &Tp::PendingOperation::finished, 
+                this, &CallChannelApprover::onChannelReady);
+    }
+}
+
+CallChannelApprover::~CallChannelApprover()
+{
+    if(m_ringingNotification)
+    {
+        m_ringingNotification->close();
+        m_ringingNotification->deleteLater();
+    }
+    deleteLater();
+}
+
+void CallChannelApprover::onChannelReady(Tp::PendingOperation* op)
+{
+    Tp::PendingReady *pendingReady = qobject_cast<Tp::PendingReady*>(op);
+    if (!pendingReady || !m_Channels.contains(pendingReady)) {
+        qWarning() << "Pending operation is not tracked" << op;
+        return;
+    }
+    
+    Tp::ChannelPtr channel = m_Channels[pendingReady];
+    Tp::CallChannelPtr callChannel = Tp::CallChannelPtr::dynamicCast(channel);
+    
+    if (channel->initiatorContact() != channel->connection()->selfContact()
+        && callChannel->callState() == Tp::CallStateInitialised
+        && !callChannel->isRequested()) {
+        qDebug() << "Blablalblalblabla";
+        callChannel->setRinging();
+    }
+    
+    QStringList actions;
+    actions << i18n("Accept") << i18n("Reject");
+    if(!m_ringingNotification) {
+        m_ringingNotification = new KNotification("ringing", KNotification::Persistent | KNotification::LoopSound, nullptr);
+    }
+    m_ringingNotification->setComponentName("plasma_dialer");
+    m_ringingNotification->setIconName("call-start");
+    m_ringingNotification->setTitle("Incoming call");
+    m_ringingNotification->setText(callChannel->targetContact()->alias());
+    m_ringingNotification->setHint("category", "x-kde.incoming-call");
+    m_ringingNotification->setActions(actions);
+    m_ringingNotification->sendEvent();
+    
+    void (KNotification::* activation)(unsigned int) = &KNotification::activated;
+    connect(m_ringingNotification, activation,
+            this, &CallChannelApprover::onNotificationAction);
+    // once we have accept or reject action from user, we forward this to call handler
+}
+
+void CallChannelApprover::onNotificationAction(unsigned int action)
+{
+    switch(action) {
+        case 1:
+            // call accepted
+            emit channelAccepted();
+            break;
+        case 2:
+            // call rejected
+            emit channelRejected();
+            break;
+        default:
+            Q_UNREACHABLE();
+            break;
+    }
+}
+
+
---
+++ approver/dispatchoperation.cpp
@@ -0,0 +1,108 @@
+/*
+    Copyright (C) 2010 Collabora Ltd. <info@collabora.co.uk>
+      @author George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
+
+    This library is free software; you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 2.1 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "dispatchoperation.h"
+#include "channelapprover.h"
+#include <TelepathyQt/PendingOperation>
+
+#include <TelepathyQt/CallChannel>
+#include <TelepathyQt/ReceivedMessage>
+
+#define TELEPHONY_SERVICE_HANDLER TP_QT_IFACE_CLIENT + ".Plasma.Dialer"
+
+DispatchOperation::DispatchOperation(const Tp::ChannelDispatchOperationPtr & dispatchOperation,
+                                     QObject *parent)
+    : QObject(parent), m_dispatchOperation(dispatchOperation)
+{
+    connect(m_dispatchOperation.data(), SIGNAL(channelLost(Tp::ChannelPtr,QString,QString)),
+            SLOT(onChannelLost(Tp::ChannelPtr,QString,QString)));
+    connect(m_dispatchOperation.data(), SIGNAL(invalidated(Tp::DBusProxy*,QString,QString)),
+            SLOT(onDispatchOperationInvalidated(Tp::DBusProxy*,QString,QString)));
+
+    Q_FOREACH(const Tp::ChannelPtr & channel, dispatchOperation->channels()) {
+        ChannelApprover *approver = ChannelApprover::create(channel, this);
+        Q_ASSERT(approver);
+
+        m_channelApprovers.insert(channel, approver);
+
+        connect(approver, SIGNAL(channelAccepted()), SLOT(onChannelAccepted()));
+        connect(approver, SIGNAL(channelRejected()), SLOT(onChannelRejected()));
+    }
+
+    Q_ASSERT(!m_channelApprovers.isEmpty());
+}
+
+DispatchOperation::~DispatchOperation()
+{
+     qDebug();
+}
+
+void DispatchOperation::onChannelLost(const Tp::ChannelPtr & channel,
+                                      const QString & errorName,
+                                      const QString & errorMessage)
+{
+    qDebug() << "Channel lost:" << errorName << errorMessage;
+
+    ChannelApprover *approver = m_channelApprovers.take(channel);
+    Q_ASSERT(approver);
+    approver->deleteLater();
+}
+
+void DispatchOperation::onDispatchOperationInvalidated(Tp::DBusProxy *proxy,
+                                                       const QString & errorName,
+                                                       const QString & errorMessage)
+{
+    Q_UNUSED(proxy);
+    qDebug() << "Dispatch operation invalidated" << errorName << errorMessage;
+    deleteLater();
+}
+
+void DispatchOperation::onChannelAccepted()
+{
+    m_dispatchOperation->handleWith(TELEPHONY_SERVICE_HANDLER);
+    for(auto channel: m_dispatchOperation->channels()) {
+        Tp::CallChannelPtr callChannel = Tp::CallChannelPtr::dynamicCast(channel);
+        if (callChannel && callChannel->callState() != Tp::CallStateActive) {
+            callChannel->accept();
+        }
+    }
+}
+
+void DispatchOperation::onChannelRejected()
+{
+    Tp::PendingOperation *operation = m_dispatchOperation->claim();
+    connect(operation, SIGNAL(finished(Tp::PendingOperation*)),
+            SLOT(onClaimFinished(Tp::PendingOperation*)));
+    Q_FOREACH(const Tp::ChannelPtr &channel, m_dispatchOperation->channels()) {
+        Tp::CallChannelPtr callChannel  = Tp::CallChannelPtr::dynamicCast(channel);
+        channel->requestClose();
+    }
+}
+
+void DispatchOperation::onClaimFinished(Tp::PendingOperation *operation)
+{
+    if (operation->isError()) {
+        qDebug() << "Claim error:" << operation->errorName() << operation->errorMessage();
+        return;
+    }
+
+    QHashIterator<Tp::ChannelPtr, ChannelApprover*> it(m_channelApprovers);
+    while(it.hasNext()) {
+        it.next();
+        it.key()->requestClose();
+    }
+}
