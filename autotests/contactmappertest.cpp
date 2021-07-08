8cf8bfb9db7995da0f24d003d283bccbcbe18c04
Nicolas Fella <nicolas.fella@gmx.de>
2021-07-08 16:20:10 +0200

    Add unit test for contactmapper
    Contactmapper is a complex piece, this helps with catching and debugging issues/regressions.
    
    It currently tests phonenumber->kpeople URI resolution, but could be extended to do other checks in the future

---
+++ autotests/contactmappertest.cpp
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: 2021 Nicolas Fella <nicolas.fella@gmx.de>
+//
+// SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
+
+#include <QTest>
+
+#include <QTemporaryFile>
+
+#include "contactmapper.h"
+#include "fakecontactsource.h"
+
+#include <KPeople/PersonPluginManager>
+#include <kpeopleprivate/personmanager_p.h>
+
+class ContactMapperTest : public QObject
+{
+    Q_OBJECT
+private Q_SLOTS:
+    void initTestCase()
+    {
+        QVERIFY(m_database.open());
+
+        // For this test we assume we are in Sweden and the implicit country code is +46
+        QLocale::setDefault(QLocale::Swedish);
+
+        // Called before the first testfunction is executed
+        PersonManager::instance(m_database.fileName());
+        m_source = new FakeContactSource(nullptr); // don't own. PersonPluginManager removes it on destruction
+        QHash<QString, KPeople::BasePersonsDataSource *> sources;
+        sources[QStringLiteral("fakesource")] = m_source;
+        KPeople::PersonPluginManager::setDataSourcePlugins(sources);
+    }
+
+    void testURI_data()
+    {
+        QTest::addColumn<QString>("number");
+        QTest::addColumn<QString>("uri");
+
+        QTest::newRow("nonexistant number") << QStringLiteral("+49123456") << QString();
+
+        QTest::newRow("contact 2, foreign country code") << QStringLiteral("+1 234 567 890") << QStringLiteral("fakesource://contact2");
+        QTest::newRow("contact 2, dashes") << QStringLiteral("+1-234-567-890") << QStringLiteral("fakesource://contact2");
+
+        QTest::newRow("contact 3") << QStringLiteral("+46 666 777 999") << QStringLiteral("fakesource://contact3");
+        QTest::newRow("contact 3, 00 instead of +") << QStringLiteral("0046 666 777 999") << QStringLiteral("fakesource://contact3");
+        QTest::newRow("contact 3, no spaces") << QStringLiteral("+46666777999") << QStringLiteral("fakesource://contact3");
+        QTest::newRow("contact 3, dashes") << QStringLiteral("+46-666-777-999") << QStringLiteral("fakesource://contact3");
+        QTest::newRow("contact 5, first number") << QStringLiteral("+46111222333") << QStringLiteral("fakesource://contact5");
+        QTest::newRow("contact 5, first number, no country code") << QStringLiteral("0111222333") << QStringLiteral("fakesource://contact5");
+        QTest::newRow("contact 5, second number") << QStringLiteral("+46333222111") << QStringLiteral("fakesource://contact5");
+        QTest::newRow("contact 5, second number, no country code") << QStringLiteral("0333222111") << QStringLiteral("fakesource://contact5");
+        QTest::newRow("invalid number") << QStringLiteral("+49yyy123456") << QString();
+    }
+
+    void testURI()
+    {
+        auto &mapper = ContactMapper::instance(true);
+
+        QFETCH(QString, number);
+        QFETCH(QString, uri);
+
+        QCOMPARE(mapper.uriForNumber(number), uri);
+    }
+
+private:
+    FakeContactSource *m_source;
+    QTemporaryFile m_database;
+};
+
+QTEST_GUILESS_MAIN(ContactMapperTest)
+
+#include "contactmappertest.moc"
