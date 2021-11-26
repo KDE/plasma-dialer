71225054d5760add7d7901b60098e0d7ba544f74
Alexey Andreyev <aa13q@ya.ru>
2021-11-26 04:50:22 +0300

    [modem-daemon] Introduce modem controller
    Contributes to #10 and #39

---
+++ modem-daemon/src/modem-controller.cpp
@@ -0,0 +1,108 @@
+// SPDX-FileCopyrightText: 2021 Alexey Andreyev <aa13q@ya.ru>
+//
+// SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
+
+#include "modem-controller.h"
+
+#include <QDebug>
+
+ModemController::ModemController(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QStringList ModemController::deviceUniList()
+{
+    return _deviceUniList;
+}
+
+void ModemController::appendDeviceUni(const QString &deviceUni)
+{
+    if (!deviceUniList().contains(deviceUni)) {
+        qDebug() << "found new device:" << deviceUni;
+        _deviceUniList.append(deviceUni);
+        Q_EMIT deviceUniListChanged(_deviceUniList);
+    }
+}
+
+void ModemController::setDeviceUniList(const QStringList &newDeviceUniList)
+{
+    if (_deviceUniList == newDeviceUniList)
+        return;
+    _deviceUniList = newDeviceUniList;
+    Q_EMIT deviceUniListChanged(_deviceUniList);
+}
+
+QString ModemController::equipmentIdentifier(const QString &deviceUni)
+{
+    Q_UNUSED(deviceUni);
+    return QString();
+}
+
+void ModemController::ussdInitiate(const QString &deviceUni, const QString &command)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(command);
+}
+
+void ModemController::ussdRespond(const QString &deviceUni, const QString &reply)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(reply);
+}
+
+void ModemController::ussdCancel(const QString &deviceUni)
+{
+    Q_UNUSED(deviceUni);
+}
+
+QString ModemController::callNumber(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+    return QString();
+}
+
+void ModemController::createCall(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+}
+
+void ModemController::acceptCall(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+}
+
+void ModemController::hangUp(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+}
+
+void ModemController::sendDtmf(const QString &deviceUni, const QString &callUni, const QString &tones)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+    Q_UNUSED(tones);
+}
+
+DialerTypes::CallDataVector ModemController::fetchCalls()
+{
+    return DialerTypes::CallDataVector();
+}
+
+DialerTypes::CallData ModemController::getCall(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+    DialerTypes::CallData callData;
+    return callData;
+}
+
+void ModemController::deleteCall(const QString &deviceUni, const QString &callUni)
+{
+    Q_UNUSED(deviceUni);
+    Q_UNUSED(callUni);
+}
---
+++ modem-daemon/src/modem-controller.h
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: 2021 Alexey Andreyev <aa13q@ya.ru>
+//
+// SPDX-License-Identifier: LicenseRef-KDE-Accepted-GPL
+#pragma once
+
+#include <kTelephonyMetaTypes/dialer-types.h>
+
+class ModemController : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit ModemController(QObject *parent = nullptr);
+Q_SIGNALS:
+    // Modem/3GPP
+    void deviceUniListChanged(const QStringList &deviceUniList);
+    void countryCodeChanged(const QString &countryCode);
+    // SMS/MMS
+    void messageAdded(const QString &deviceUni, const QVariantMap &message, bool received);
+    // USSD
+    void ussdNotificationReceived(const QString &deviceUni, const QString &message);
+    void ussdRequestReceived(const QString &deviceUni, const QString &message);
+    void ussdInitiateComplete(const QString &deviceUni, const QString &command);
+    void ussdStateChanged(const QString &deviceUni, const QString &state);
+    // Calls
+    void callAdded(const QString &deviceUni,
+                   const QString &callUni,
+                   const DialerTypes::CallDirection &callDirection,
+                   const DialerTypes::CallState &callState,
+                   const DialerTypes::CallStateReason &callStateReason,
+                   const QString communicationWith);
+    void callDeleted(const QString &deviceUni, const QString &callUni);
+    void callStateChanged(const QString &deviceUni,
+                          const QString &callUni,
+                          const DialerTypes::CallDirection &callDirection,
+                          const DialerTypes::CallState &callState,
+                          const DialerTypes::CallStateReason &callStateReason);
+
+public Q_SLOTS:
+    // Device
+    QStringList deviceUniList();
+    void appendDeviceUni(const QString &deviceUni);
+    void setDeviceUniList(const QStringList &newDeviceUniList);
+    virtual QString equipmentIdentifier(const QString &deviceUni);
+    // USSD
+    virtual void ussdInitiate(const QString &deviceUni, const QString &command);
+    virtual void ussdRespond(const QString &deviceUni, const QString &reply);
+    virtual void ussdCancel(const QString &deviceUni);
+    // Calls
+    virtual QString callNumber(const QString &deviceUni, const QString &callUni);
+    virtual void createCall(const QString &deviceUni, const QString &callUni);
+    virtual void acceptCall(const QString &deviceUni, const QString &callUni);
+    virtual void hangUp(const QString &deviceUni, const QString &callUni);
+    virtual void sendDtmf(const QString &deviceUni, const QString &callUni, const QString &tones);
+    virtual DialerTypes::CallDataVector fetchCalls();
+    virtual DialerTypes::CallData getCall(const QString &deviceUni, const QString &callUni);
+    virtual void deleteCall(const QString &deviceUni, const QString &callUni);
+
+private:
+    QStringList _deviceUniList;
+};
