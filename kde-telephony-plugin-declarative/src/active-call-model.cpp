51949ecaf163463879b5db56c97de40223f40b0d
alexandertrofimov <trofimov_al@mail.ru>
2022-08-06 13:56:58 +0300

    Emit call state of first call to get incoming call RingingIn state if application was not launched

--- kde-telephony-plugin-declarative/src/active-call-model.cpp
+++ kde-telephony-plugin-declarative/src/active-call-model.cpp
@@ -139,6 +139,9 @@ void ActiveCallModel::onFetchedCallsChanged(const DialerTypes::CallDataVector &f
     _calls = fetchedCalls;
     endResetModel();
     bool active = (_calls.size() > 0);
+    // TODO (Alexander Trofimov): Set Call State to get incoming call RingingIn state if application was not launched
+    if (_calls[0].state == DialerTypes::CallState::RingingIn)
+        setCallState(_calls[0].state);
     setActive(active);
 }
 
