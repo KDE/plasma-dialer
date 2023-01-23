6a6ed1744389992248e141e897f643ca36b38a91
Marco Mattiolo <marco.mattiolo@hotmail.it>
2023-01-23 20:19:40 +0000

    Fix typo: notification-manager.* is licensed with some kind of LGPL

--- kde-telephony-daemon/src/notification-manager.cpp
+++ kde-telephony-daemon/src/notification-manager.cpp
@@ -2,7 +2,7 @@
 // SPDX-FileCopyrightText: 2020 Jonah Brüchert <jbb@kaidan.im>
 // SPDX-FileCopyrightText: 2021 Nicolas Fella <nicolas.fella@gmx.de>
 // SPDX-FileCopyrightText: 2021 Alexey Andreyev <aa13q@ya.ru>
-// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 
 #include "notification-manager.h"
 #include "config.h"
