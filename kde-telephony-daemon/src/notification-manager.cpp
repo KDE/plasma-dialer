KDE/plasma-dialer

commit 68eab7f43051d721facc1b1dfff90edaf6fd9731

[kde-telephony-daemon] Introduce as subproject
A daemon for background tracking the modem events to:

- Enable audio channels when needed
- Show notifications
- Share KDE-related Telephony services via session DBus

Power management (wake up the device) is supported by eg25-manager (via ModemManager).

Contributes to #10 and #39

kde-telephony-daemon/src/notification-manager.cpp
@@ -1,0 +1,144 @@
+// SPDX-FileCopyrightText: 2020-2021 Bhushan Shah <bshah@kde.org>
+// SPDX-FileCopyrightText: 2020 Jonah Brüchert <jbb@kaidan.im>
+// SPDX-FileCopyrightText: 2021 Nicolas Fella <nicolas.fella@gmx.de>
+// SPDX-FileCopyrightText: 2021 Alexey Andreyev <aa13q@ya.ru>
+// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
+#include "notification-manager.h"
+
+#include <KLocalizedString>
+
+NotificationManager::NotificationManager(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void NotificationManager::setCallUtils(org::kde::telephony::CallUtils *callUtils)
+{
+    qDebug() << Q_FUNC_INFO;
+    _callUtils = callUtils;
+
+    connect(_callUtils, &org::kde::telephony::CallUtils::callAdded, this, &NotificationManager::onCallAdded);
+    connect(_callUtils, &org::kde::telephony::CallUtils::callStateChanged, this, &NotificationManager::onCallStateChanged);
+    connect(_callUtils, &org::kde::telephony::CallUtils::callDeleted, this, &NotificationManager::onCallDeleted);
+}
+
+void NotificationManager::setContactUtils(ContactUtils *contactUtils)
+{
+    _contactUtils = contactUtils;
+}
+
+void NotificationManager::onCallAdded(const QString &deviceUni,
+                                      const QString &callUni,
+                                      const DialerTypes::CallDirection &callDirection,
+                                      const DialerTypes::CallState &callState,
+                                      const DialerTypes::CallStateReason &callStateReason,
+                                      const QString communicationWith)
+{
+    qDebug() << Q_FUNC_INFO << "call added" << deviceUni << callUni << callDirection << callState << callStateReason << communicationWith;
+    if (callDirection == DialerTypes::CallDirection::Incoming) {
+        if (callState == DialerTypes::CallState::RingingIn) {
+            openRingingNotification(deviceUni, callUni, communicationWith);
+        }
+    }
+}
+
+void NotificationManager::onCallDeleted(const QString &deviceUni, const QString &callUni)
+{
+    qDebug() << Q_FUNC_INFO << "call deleted" << deviceUni << callUni;
+    closeRingingNotification();
+}
+
+void NotificationManager::onCallStateChanged(const QString &deviceUni,
+                                             const QString &callUni,
+                                             const DialerTypes::CallDirection &callDirection,
+                                             const DialerTypes::CallState &callState,
+                                             const DialerTypes::CallStateReason &callStateReason)
+{
+    qDebug() << Q_FUNC_INFO << "call state changed:" << deviceUni << callUni << callDirection << callState << callStateReason;
+}
+
+void NotificationManager::openRingingNotification(const QString &deviceUni, const QString &callUni, const QString communicationWith)
+{
+    QString callerDisplayName = _contactUtils->displayString(communicationWith);
+
+    QStringList actions;
+    actions << i18n("Accept") << i18n("Reject");
+    qDebug() << Q_FUNC_INFO << _ringingNotification;
+    if (!_ringingNotification) {
+        qDebug() << Q_FUNC_INFO << "new notification";
+        _ringingNotification = new KNotification(QStringLiteral("ringing"), KNotification::Persistent | KNotification::LoopSound, nullptr);
+        void (KNotification::*activation)(unsigned int) = &KNotification::activated;
+        connect(_ringingNotification, activation, this, &NotificationManager::onNotificationAction);
+    }
+    _ringingNotification->setUrgency(KNotification::CriticalUrgency);
+    _ringingNotification->setComponentName(QStringLiteral("plasma_dialer"));
+    // _ringingNotification->setPixmap(person.photo());
+    _ringingNotification->setTitle(i18n("Incoming call"));
+    _ringingNotification->setText(callerDisplayName);
+    // this will be used by the notification applet to show custom notification UI
+    // with swipe decision.
+    _ringingNotification->setHint(QStringLiteral("category"), QStringLiteral("x-kde.incoming-call"));
+    _ringingNotification->setActions(actions);
+    _ringingNotification->addContext(QStringLiteral("deviceUni"), deviceUni);
+    _ringingNotification->addContext(QStringLiteral("callUni"), callUni);
+    _ringingNotification->sendEvent();
+}
+
+void NotificationManager::closeRingingNotification()
+{
+    if (!_ringingNotification) {
+        return;
+    }
+    _ringingNotification->disconnect();
+    _ringingNotification->close();
+    _ringingNotification = nullptr;
+}
+
+void NotificationManager::accept(const QString &deviceUni, const QString &callUni)
+{
+    QDBusPendingReply<> reply = _callUtils->accept(deviceUni, callUni);
+    reply.waitForFinished();
+    if (reply.isError()) {
+        qDebug() << Q_FUNC_INFO << reply.error();
+    }
+    closeRingingNotification();
+}
+
+void NotificationManager::hangUp(const QString &deviceUni, const QString &callUni)
+{
+    QDBusPendingReply<> reply = _callUtils->hangUp(deviceUni, callUni);
+    reply.waitForFinished();
+    if (reply.isError()) {
+        qDebug() << Q_FUNC_INFO << reply.error();
+    }
+    closeRingingNotification();
+}
+
+void NotificationManager::onNotificationAction(unsigned int action)
+{
+    qDebug() << Q_FUNC_INFO << action;
+    QString deviceUni;
+    QString callUni;
+    KNotification::ContextList notificationContexts = _ringingNotification->contexts();
+    for (const auto &context : notificationContexts) {
+        if (context.first == QStringLiteral("deviceUni")) {
+            deviceUni = context.second;
+        }
+        if (context.first == QStringLiteral("callUni")) {
+            callUni = context.second;
+        }
+    }
+    switch (action) {
+    case 1:
+        accept(deviceUni, callUni);
+        QProcess::startDetached(QStringLiteral("plasmaphonedialer"), QStringList{});
+        break;
+    case 2:
+        hangUp(deviceUni, callUni);
+        break;
+    default:
+        Q_UNREACHABLE();
+        break;
+    }
+}

approver/CMakeLists.txt
@@ -1,24 +1,0 @@
-add_executable(plasma-telepathy-approver
-    main.cpp
-    phoneapprover.cpp
-    dispatchoperation.cpp
-    channelapprover.cpp
-    callchannelapprover.cpp
-)
-
-target_include_directories(plasma-telepathy-approver PRIVATE ${CMAKE_BINARY_DIR})
-
-target_link_libraries(plasma-telepathy-approver
-    Qt5::Core
-    Qt5::Quick
-    KF5::CoreAddons
-    KF5::DBusAddons
-    KF5::I18n
-    KF5::Notifications
-    TelepathyQt5::Core
-    dialer
-)
-
-install(TARGETS plasma-telepathy-approver ${INSTALL_TARGETS_DEFAULT_ARGS})
-
-ecm_install_configured_files(INPUT org.freedesktop.Telepathy.Client.Plasma.Approver.service.in DESTINATION ${DBUS_SERVICES_INSTALL_DIR} @ONLY)

approver/callchannelapprover.cpp
@@ -1,100 +1,0 @@
-// SPDX-FileCopyrightText: 2020-2021 Bhushan Shah <bshah@kde.org>
-// SPDX-FileCopyrightText: 2020 Jonah Brüchert <jbb@kaidan.im>
-// SPDX-FileCopyrightText: 2021 Nicolas Fella <nicolas.fella@gmx.de>
-// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
-
-#include "callchannelapprover.h"
-
-#include <KLocalizedString>
-
-#include <KPeople/PersonData>
-
-#include <TelepathyQt/CallChannel>
-#include <TelepathyQt/ChannelClassSpec>
-#include <TelepathyQt/ClientRegistrar>
-#include <TelepathyQt/PendingReady>
-
-#include "contactmapper.h"
-
-CallChannelApprover::CallChannelApprover(const Tp::CallChannelPtr &channel, QObject *parent)
-    : ChannelApprover(parent)
-{
-    if (!channel.isNull()) {
-        Tp::PendingReady *pendingReady = channel->becomeReady(Tp::Features() << Tp::CallChannel::FeatureCore << Tp::CallChannel::FeatureCallState);
-        m_Channels[pendingReady] = channel;
-        connect(pendingReady, &Tp::PendingOperation::finished, this, &CallChannelApprover::onChannelReady);
-    }
-}
-
-CallChannelApprover::~CallChannelApprover()
-{
-    deleteLater();
-}
-
-void CallChannelApprover::onChannelReady(Tp::PendingOperation *op)
-{
-    auto *pendingReady = qobject_cast<Tp::PendingReady *>(op);
-    if (!pendingReady || !m_Channels.contains(pendingReady)) {
-        qWarning() << "Pending operation is not tracked" << op;
-        return;
-    }
-
-    Tp::ChannelPtr channel = m_Channels.value(pendingReady);
-    Tp::CallChannelPtr callChannel = Tp::CallChannelPtr::dynamicCast(channel);
-
-    if (channel->initiatorContact() != channel->connection()->selfContact() && callChannel->callState() == Tp::CallStateInitialised
-        && !callChannel->isRequested()) {
-        callChannel->setRinging();
-    }
-
-    KPeople::PersonData person(ContactMapper::instance().uriForNumber(callChannel->targetContact()->alias()));
-
-    const QString callerDisplayName = !person.name().isEmpty() ? person.name() : callChannel->targetContact()->alias();
-
-    QStringList actions;
-    actions << i18n("Accept") << i18n("Reject");
-    if (!m_ringingNotification) {
-        m_ringingNotification = new KNotification(QStringLiteral("ringing"), KNotification::Persistent | KNotification::LoopSound, nullptr);
-    }
-    m_ringingNotification->setUrgency(KNotification::CriticalUrgency);
-    m_ringingNotification->setComponentName(QStringLiteral("plasma_dialer"));
-    m_ringingNotification->setPixmap(person.photo());
-    m_ringingNotification->setTitle(i18n("Incoming call"));
-    m_ringingNotification->setText(callerDisplayName);
-    // this will be used by the notification applet to show custom notification UI
-    // with swipe decision.
-    m_ringingNotification->setHint(QStringLiteral("category"), "x-kde.incoming-call");
-    m_ringingNotification->setActions(actions);
-    m_ringingNotification->sendEvent();
-
-    QDBusMessage wakeupCall = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Solid.PowerManagement"),
-                                                             QStringLiteral("/org/kde/Solid/PowerManagement"),
-                                                             QStringLiteral("org.kde.Solid.PowerManagement"),
-                                                             QStringLiteral("wakeup"));
-    QDBusConnection::sessionBus().call(wakeupCall);
-
-    connect(callChannel.data(), &Tp::CallChannel::callStateChanged, this, [=, this](Tp::CallState state) {
-        if (state == Tp::CallStateEnded) {
-            m_ringingNotification->close();
-        }
-    });
-    void (KNotification::*activation)(unsigned int) = &KNotification::activated;
-    connect(m_ringingNotification, activation, this, &CallChannelApprover::onNotificationAction);
-}
-
-void CallChannelApprover::onNotificationAction(unsigned int action)
-{
-    switch (action) {
-    case 1:
-        // call accepted
-        emit channelAccepted();
-        break;
-    case 2:
-        // call rejected
-        emit channelRejected();
-        break;
-    default:
-        Q_UNREACHABLE();
-        break;
-    }
-}

approver/callchannelapprover.h
@@ -1,29 +1,0 @@
-// SPDX-FileCopyrightText: 2020 Bhushan Shah <bshah@kde.org>
-// SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
-
-#ifndef CALLCHANNELAPPROVER_H
-#define CALLCHANNELAPPROVER_H
-
-#include "channelapprover.h"
-
-#include <TelepathyQt/CallChannel>
-
-#include <KNotification>
-
-class CallChannelApprover : public ChannelApprover
-{
-    Q_OBJECT
-public:
-    CallChannelApprover(const Tp::CallChannelPtr &channel, QObject *parent);
-    ~CallChannelApprover();
-
-private slots:
-    void onChannelReady(Tp::PendingOperation *op);
-    void onNotificationAction(unsigned int action);
-
-private:
-    QMap<Tp::PendingOperation *, Tp::ChannelPtr> m_Channels;
-    KNotification *m_ringingNotification = nullptr;
-};
-
-#endif // CALLCHANNELAPPROVER_H

approver/channelapprover.cpp
@@ -1,14 +1,0 @@
-// SPDX-FileCopyrightText: 2010 Collabora Ltd. <info@collabora.co.uk>
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-#include "channelapprover.h"
-
-#include "callchannelapprover.h"
-
-ChannelApprover *ChannelApprover::create(const Tp::ChannelPtr &channel, QObject *parent)
-{
-    if (channel->channelType() == TP_QT_IFACE_CHANNEL_TYPE_CALL) {
-        return new CallChannelApprover(Tp::CallChannelPtr::dynamicCast(channel), parent);
-    }
-    return nullptr;
-}

approver/channelapprover.h
@@ -1,30 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2010 Collabora Ltd. <info@collabora.co.uk>
-      @author George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
-
-    SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-#ifndef CHANNELAPPROVER_H
-#define CHANNELAPPROVER_H
-
-#include <QtCore/QObject>
-#include <TelepathyQt/Channel>
-
-class ChannelApprover : public QObject
-{
-    Q_OBJECT
-public:
-    static ChannelApprover *create(const Tp::ChannelPtr &channel, QObject *parent);
-
-Q_SIGNALS:
-    void channelAccepted();
-    void channelRejected();
-
-protected:
-    inline ChannelApprover(QObject *parent)
-        : QObject(parent)
-    {
-    }
-};
-
-#endif // CHANNELAPPROVER_H

approver/dispatchoperation.cpp
@@ -1,91 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2010 Collabora Ltd. <info@collabora.co.uk>
-    SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-#include "dispatchoperation.h"
-#include "channelapprover.h"
-#include <TelepathyQt/PendingOperation>
-
-#include <TelepathyQt/CallChannel>
-#include <TelepathyQt/ReceivedMessage>
-
-#define TELEPHONY_SERVICE_HANDLER TP_QT_IFACE_CLIENT + ".Plasma.Dialer"
-
-DispatchOperation::DispatchOperation(const Tp::ChannelDispatchOperationPtr &dispatchOperation, QObject *parent)
-    : QObject(parent)
-    , m_dispatchOperation(dispatchOperation)
-{
-    connect(m_dispatchOperation.data(), &Tp::ChannelDispatchOperation::channelLost, this, &DispatchOperation::onChannelLost);
-    connect(m_dispatchOperation.data(), &Tp::ChannelDispatchOperation::invalidated, this, &DispatchOperation::onDispatchOperationInvalidated);
-
-    const auto channels = dispatchOperation->channels();
-    for (const Tp::ChannelPtr &channel : channels) {
-        ChannelApprover *approver = ChannelApprover::create(channel, this);
-        Q_ASSERT(approver);
-
-        m_channelApprovers.insert(channel, approver);
-
-        connect(approver, &ChannelApprover::channelAccepted, this, &DispatchOperation::onChannelAccepted);
-        connect(approver, &ChannelApprover::channelRejected, this, &DispatchOperation::onChannelRejected);
-    }
-
-    Q_ASSERT(!m_channelApprovers.isEmpty());
-}
-
-DispatchOperation::~DispatchOperation()
-{
-    qDebug();
-}
-
-void DispatchOperation::onChannelLost(const Tp::ChannelPtr &channel, const QString &errorName, const QString &errorMessage)
-{
-    qDebug() << "Channel lost:" << errorName << errorMessage;
-
-    ChannelApprover *approver = m_channelApprovers.take(channel);
-    Q_ASSERT(approver);
-    approver->deleteLater();
-}
-
-void DispatchOperation::onDispatchOperationInvalidated(Tp::DBusProxy *proxy, const QString &errorName, const QString &errorMessage)
-{
-    Q_UNUSED(proxy);
-    qDebug() << "Dispatch operation invalidated" << errorName << errorMessage;
-    deleteLater();
-}
-
-void DispatchOperation::onChannelAccepted()
-{
-    m_dispatchOperation->handleWith(TELEPHONY_SERVICE_HANDLER);
-    const auto channels = m_dispatchOperation->channels();
-    for (const auto &channel : m_dispatchOperation->channels()) {
-        Tp::CallChannelPtr callChannel = Tp::CallChannelPtr::dynamicCast(channel);
-        if (callChannel && callChannel->callState() != Tp::CallStateActive) {
-            callChannel->accept();
-        }
-    }
-}
-
-void DispatchOperation::onChannelRejected()
-{
-    Tp::PendingOperation *operation = m_dispatchOperation->claim();
-    connect(operation, &Tp::PendingOperation::finished, this, &DispatchOperation::onClaimFinished);
-    const auto channels = m_dispatchOperation->channels();
-    for (const Tp::ChannelPtr &channel : channels) {
-        Tp::CallChannelPtr callChannel = Tp::CallChannelPtr::dynamicCast(channel);
-        callChannel->hangup(Tp::CallStateChangeReasonRejected, TP_QT_ERROR_REJECTED);
-    }
-}
-
-void DispatchOperation::onClaimFinished(Tp::PendingOperation *operation)
-{
-    if (operation->isError()) {
-        qDebug() << "Claim error:" << operation->errorName() << operation->errorMessage();
-        return;
-    }
-
-    QHashIterator<Tp::ChannelPtr, ChannelApprover *> it(m_channelApprovers);
-    while (it.hasNext()) {
-        it.next();
-        it.key()->requestClose();
-    }
-}

approver/dispatchoperation.h
@@ -1,34 +1,0 @@
-/*
-    SPDX-FileCopyrightText: 2010 Collabora Ltd. <info@collabora.co.uk>
-      @author George Kiagiadakis <george.kiagiadakis@collabora.co.uk>
-
-    SPDX-License-Identifier: LGPL-2.1-or-later
-*/
-#ifndef DISPATCHOPERATION_H
-#define DISPATCHOPERATION_H
-
-#include <QtCore/QObject>
-#include <TelepathyQt/ChannelDispatchOperation>
-
-class ChannelApprover;
-
-class DispatchOperation : public QObject
-{
-    Q_OBJECT
-public:
-    DispatchOperation(const Tp::ChannelDispatchOperationPtr &dispatchOperation, QObject *parent);
-    ~DispatchOperation() override;
-
-private Q_SLOTS:
-    void onChannelLost(const Tp::ChannelPtr &channel, const QString &errorName, const QString &errorMessage);
-    void onDispatchOperationInvalidated(Tp::DBusProxy *proxy, const QString &errorName, const QString &errorMessage);
-    void onChannelAccepted();
-    void onChannelRejected();
-    void onClaimFinished(Tp::PendingOperation *operation);
-
-private:
-    Tp::ChannelDispatchOperationPtr m_dispatchOperation;
-    QHash<Tp::ChannelPtr, ChannelApprover *> m_channelApprovers;
-};
-
-#endif // DISPATCHOPERATION_H

commit c047c9447e31c12ee33c3590c59a83f7edeee6be

notification-manager: re-enable waking up display
This was lost in refactoring it seems, it is required to turn display on
when you are not waking up from suspend.

Fixes: 68eab7f43051 "[kde-telephony-daemon] Introduce as subproject"

kde-telephony-daemon/src/notification-manager.cpp
@@ -11,6 +11,7 @@
 #include <KLocalizedString>
 #include <QDBusConnection>
 #include <QTimer>
+#include <qdbusmessage.h>
 
 static bool getScreenSaverActive()
 {
@@ -265,6 +266,12 @@ void NotificationManager::handleIncomingCall(const QString &deviceUni, const QSt
     if (callerDisplay.isEmpty()) {
         callerDisplay = i18n("No Caller ID");
     }
+
+    QDBusMessage wakeupCall = QDBusMessage::createMethodCall(QStringLiteral("org.kde.Solid.PowerManagement"),
+                                                             QStringLiteral("/org/kde/Solid/PowerManagement"),
+                                                             QStringLiteral("org.kde.Solid.PowerManagement"),
+                                                             QStringLiteral("wakeup"));
+    QDBusConnection::sessionBus().call(wakeupCall);
 
     if (!skipNotification) {
         openRingingNotification(deviceUni, callUni, callerDisplay, notificationEvent);